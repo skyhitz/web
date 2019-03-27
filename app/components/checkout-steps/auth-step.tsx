140b30da5ee0d264f89b81ecf7cf01f1663bca67
alejo <alejo@splice.com>
2019-03-27 14:34:00 -0500

    take out facebook login

--- app/components/checkout-steps/auth-step.tsx
+++ app/components/checkout-steps/auth-step.tsx
@@ -1,9 +1,6 @@
 import React from 'react';
-import FacebookLogin from 'react-facebook-login';
-import { Config } from 'app/config';
 import './auth-step.scss';
 import EmailAuthForm from '../email-auth-form/email-auth-form';
-import AppLink from '../app-link/app-link';
 import { inject } from 'mobx-react';
 
 @inject((stores: any) => ({
@@ -31,28 +28,8 @@ export class AuthStep extends React.Component<any, any> {
 
   render() {
     return (
-      <div className='auth-step-container'>
-        <div className='signin-column'>
-          {this.state.signInError ? (
-            <div className='facebook-signin-error'>
-              <AppLink>{this.state.signInError}</AppLink>
-            </div>
-          ) : (
-            ''
-          )}
-          <FacebookLogin
-            appId={Config.FACEBOOK_APP_ID}
-            autoLoad={true}
-            cssClass='facebook-button'
-            fields='email'
-            icon='fa-facebook'
-            callback={this.handleResponse.bind(this)}
-          />
-        </div>
-        <div className='hor-divider'>or</div>
-        <div className='signin-column'>
-          <EmailAuthForm />
-        </div>
+      <div className="auth-step-container">
+        <EmailAuthForm />
       </div>
     );
   }
