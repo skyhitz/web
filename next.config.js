fbe94eb18856e7ce108638c58fd629aae087a21b
alejo <alejo@splice.com>
2019-04-14 12:47:12 -0500

    take out phase development buld

--- next.config.js
+++ next.config.js
@@ -94,7 +94,7 @@ module.exports = withPlugins(
           ]
         }
       },
-      [PHASE_PRODUCTION_BUILD, PHASE_DEVELOPMENT_SERVER]
+      [PHASE_PRODUCTION_BUILD]
     ],
     [
       optional(() => require('@zeit/next-bundle-analyzer')),
